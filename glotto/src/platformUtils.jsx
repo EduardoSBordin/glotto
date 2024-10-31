afdbf6b809d53e457b2798ce534747f9fc9e8fd0
Eduardo Bordin <dudubordin26@gmail.com>
2024-10-31 20:58:31 -0300

    v19

--- glotto/src/platformUtils.jsx
+++
@@ -1,9 +0,0 @@
-// src/platformUtils.js
-export const getPlatform = () => {
-    if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-        return "ios";
-    } else if (/Android/i.test(navigator.userAgent)) {
-        return "android";
-    }
-    return "other";
-};
